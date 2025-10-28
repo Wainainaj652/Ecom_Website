f5f888f91193ebe2cdbaf1bff4661e1125375386
James Wainaina <tomkim590@gmail.com>
2025-10-28 15:29:46 +0300

    firstcommit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>
+      <h2  className="text-center" style={{ padding: "10rem" }}> CART PAGE</h2>
+    </div>
+  )
+}
+
+export default Cart
